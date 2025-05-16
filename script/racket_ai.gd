52933183849e30c86413898608a01aede819fc03
mapopi-mm <mm@mapopi.net>
2025-05-16 14:37:01 -0300

    Added enemy AI

---
+++ script/racket_ai.gd
@@ -0,0 +1,13 @@
+extends CharacterBody2D
+
+var speed: float = 150
+var direction: Vector2 = Vector2.ZERO
+
+@onready var ball: CharacterBody2D = $"../Ball"
+
+
+func _physics_process(delta: float) -> void:
+	direction.y = global_position.direction_to(ball.global_position).y
+	direction = direction.normalized()
+	velocity = direction * speed
+	move_and_slide()
