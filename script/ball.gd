a20340ed229712f35406ea9ff9610a02c252d696
mapopi-mm <mm@mapopi.net>
2025-05-16 14:16:25 -0300

    Added walls and collision

--- script/ball.gd
+++ script/ball.gd
@@ -1,9 +1,13 @@
 extends CharacterBody2D
 
 var speed: float = 300
-var direction: Vector2 = Vector2(-1, 0)
+var direction: Vector2 = Vector2(-1, -1).normalized()
 
 
 func _physics_process(delta: float) -> void:
 	velocity = direction * speed
 	move_and_slide()
+	
+	if get_slide_collision_count() > 0:
+		var normal = get_slide_collision(0).get_normal()
+		direction = direction.bounce(normal)
