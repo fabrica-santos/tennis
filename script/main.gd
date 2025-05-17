96741224856e7254c677e14dab2a464706f0e481
mapopi-mm <mm@mapopi.net>
2025-05-16 22:24:39 -0300

    Added scoring logic

--- script/main.gd
+++ script/main.gd
@@ -1,5 +1,8 @@
 extends Node2D
 
+var score1: int = 0
+var score2: int = 0
+
 @onready var ball: CharacterBody2D = $Ball
 @onready var player_scored_sound: AudioStreamPlayer = $PlayerScoredSound
 @onready var enemy_scored_sound: AudioStreamPlayer = $EnemyScoredSound
@@ -8,8 +11,16 @@ extends Node2D
 func _on_goal_1_body_entered(body: Node2D) -> void:
 	ball.global_position = Vector2(320, 180)
 	enemy_scored_sound.play()
+	score2 += 1
+	update_score()
 
 
 func _on_goal_2_body_entered(body: Node2D) -> void:
 	ball.global_position = Vector2(320, 180)
 	player_scored_sound.play()
+	score1 += 1
+	update_score()
+
+
+func update_score() -> void:
+	print(score1, "/", score2)
